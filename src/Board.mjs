50adb51292c8eff8c50d90fd1ba17422d33a24d4
zsonbi <zsomborder@gmail.com>
2025-01-18 21:08:46 +0200

    Level 1 done

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,9 +1,11 @@
 export class Board {
   width;
   height;
+  grid;
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.grid=Array.from({ length: this.height }, () => Array(this.width).fill('.'));
   }
 
 
@@ -12,7 +14,7 @@ export class Board {
     let res="";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        res+='.';
+        res+=this.grid[i][j];
       }
       res+="\n";
     }
