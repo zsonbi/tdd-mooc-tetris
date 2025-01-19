f8c726cc428dcdf8ba59095bd9d41ff519601209
zsonbi <zsomborder@gmail.com>
2025-01-19 13:25:14 +0200

    formated left rotate

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -9,12 +9,12 @@ export class RotatingShape {
     }
 
     toString() {
-        return this.grid.map(row => row.join('')).join('\n')+'\n';
+        return this.grid.map(row => row.join('')).join('\n') + '\n';
     }
 
-    rotateRight(){
+    rotateRight() {
         gridSize = this.grid.size;
-        rotatedGrid=Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
+        rotatedGrid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
         for (let row = 0; row < size; row++) {
             for (let col = 0; col < size; col++) {
                 rotatedGrid[col][gridSize - row - 1] = this.grid[row][col];
@@ -24,12 +24,14 @@ export class RotatingShape {
         return rotated;
     }
 
-    rotateLeft(){
+    rotateLeft() {
         gridSize = this.grid.size;
-        rotatedGrid=Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
+        rotatedGrid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
         for (let row = 0; row < size; row++) {
             for (let col = 0; col < size; col++) {
-                rotatedGrid[size - col - 1][row] = this.grid[row][col];}}
+                rotatedGrid[size - col - 1][row] = this.grid[row][col];
+            }
+        }
         const rotated = new RotatingShape(rotatedGrid)
         return rotated;
     }
