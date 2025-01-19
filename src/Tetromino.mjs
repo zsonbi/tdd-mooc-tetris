1f9143445b122098b85090a6861621a9c5ffd554
zsonbi <zsomborder@gmail.com>
2025-01-19 14:00:17 +0200

    Reformating

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -14,19 +14,23 @@ export class Tetromino {
     let rotatedGrid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
      for (let row = 0; row < gridSize; row++) {
          for (let col = 0; col < gridSize; col++) {
-             rotatedGrid[col][gridSize - row - 1] = this.grid[row][col];}}
+             rotatedGrid[col][gridSize - row - 1] = this.grid[row][col];
+            }
+          }
      return new Tetromino(rotatedGrid);
- }
+  }
   
  rotateLeft() {
   let gridSize = this.grid.length;
   let rotatedGrid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));;
    for (let row = 0; row < gridSize; row++) {
        for (let col = 0; col < gridSize; col++) {
-        rotatedGrid[gridSize - col - 1][row] = this.grid[row][col];}}
+        rotatedGrid[gridSize - col - 1][row] = this.grid[row][col];
+      }
+    }
    return new Tetromino(rotatedGrid);
-}
+  }
   toString() {
     return this.grid.map(row => row.join('')).join('\n') + '\n';
-}
+  }
 }
