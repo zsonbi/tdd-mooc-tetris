8b539aa5dd58286d0080d6ca0f4d4f3e865e788d
zsonbi <zsomborder@gmail.com>
2025-02-03 22:55:07 +0200

    added score increase

--- src/ScoringSystem.mjs
+++ src/ScoringSystem.mjs
@@ -1,9 +1,13 @@
 class ScoringSystem {
     constructor() {
         this.score = 0;
+        this.scores=[0, 40, 100, 300, 1200];
     }
 
-    
+        onCleared(numRows) {
+            this.score += this.scores[numRows];
+            console.log("New score "+this.score);
+        }
 
     getScore() {
         return this.score;
