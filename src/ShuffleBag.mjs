c393f94934206c6d53cc2d88acc3a6a6bed3b3c0
zsonbi <zsomborder@gmail.com>
2025-02-04 00:53:16 +0200

    Update ShuffleBag.mjs

--- src/ShuffleBag.mjs
+++ src/ShuffleBag.mjs
@@ -2,5 +2,15 @@ export class ShuffleBag{
 
     constructor(shapes){
         this.shapes = shapes;
+        this.bag=[];
+        this.bagIndex=0;
+        this.shuffle();
+    }
+
+    pop(){
+
+    }
+    shuffle(){
+
     }
 }
